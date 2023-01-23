c9e32d7c031fd44e7d9ed8d296b029962fb66ad9
SirPudwhip <spamfolder3399@gmail.com>
2023-01-23 12:35:44 -0500

    Added function looping over table for inputs and checking work

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-let whole = [];
-
-for (var i=0; i <6; i++) {
-    const num = Math.floor(Math.random() * 9);
-    whole.push(num);
+function makeNumber() {
+    const whole = [];
+    for (var i=0; i <6; i++) {
+        const num = Math.floor(Math.random() * 9);
+        whole.push(num);
+    }
+    return whole.join("");
 }
 
-console.log(whole)
+//Get all uhhh the target HTML elements 
+const columns = document.getElementsByClassName("target");
+
+const results = document.getElementsByClassName("result");
+
+//set all of the target values 
+for (let i=0; i<columns.length; i++) {
+    columns[i].innerHTML = makeNumber();
+}
+
+//get all of the input HTML elements 
+const inputs = document.getElementsByTagName("input");
+
+function checkMe() {
+    for (let i=0; i<inputs.length; i++) {
+        if (columns[i].textContent === inputs[i].value) {
+            results[i].innerHTML = "You are a juicy boy!";
+            results[i].classList.remove('wrong');
+        }
+        else {
+            results[i].innerHTML = "You will NEVER be a juicy boy";
+            results[i].classList.add('wrong');
+        }
+    }
+}
